021b387e68d9f0ae0b27fa97091d21b8a7014c13
AndresCR24 <c-andres-98@hotmail.com>
2024-08-30 07:50:52 -0500

    Initial Commit

---
+++ BibliotecaIphoneIpad/BibliotecaIphoneIpadApp.swift
@@ -0,0 +1,17 @@
+//
+//  BibliotecaIphoneIpadApp.swift
+//  BibliotecaIphoneIpad
+//
+//  Created by Andres David Cardenas Ramirez on 30/08/24.
+//
+
+import SwiftUI
+
+@main
+struct BibliotecaIphoneIpadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BibliotecaIphoneIpad/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BibliotecaIphoneIpad
+//
+//  Created by Andres David Cardenas Ramirez on 30/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
